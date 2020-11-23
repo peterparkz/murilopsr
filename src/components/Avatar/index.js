cf5526ceaecda9332e725a3373331e8cd4b33c33
peterparkz <74876517+peterparkz@users.noreply.github.com>
2020-11-22 23:26:54 -0300

    Update index.js

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -7,7 +7,7 @@ const Avatar = () => {
   const { avatarImage } = useStaticQuery(
     graphql`
       query {
-        avatarImage: file(relativePath: { eq: "profile-photo-harry.jpg" }) {
+        avatarImage: file(relativePath: { eq: "https://pbs.twimg.com/profile_images/1325865754386960384/lfyVJ5qq_400x400.png" }) {
           childImageSharp {
             fluid(maxWidth: 60) {
               ...GatsbyImageSharpFluid
